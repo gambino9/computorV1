cf38c404b9cd63c5e8d80fb53d5a1483a2f693c0
Lamia <lboukrou@student.42.fr>
2022-08-03 19:43:24 +0200

    [DEV] Add some polynomial strings for tests purpose

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ if __name__ == "__main__":
     polynomial_6 = '5 * X^0 = -5 * X^0'
     polynomial_7 = '9 - 8X^0 - 6X^1 + 0X^2 - 5.6 * X^3 = 3 * X^0'  # forme naturelle et non naturelle melangees
 
-    poly_subj = '5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0'  # TODO : FAUX
-    poly_subj_1 = '5 * X^0 + 4 * X^1 = 4 * X^0'  # FAUX
+    poly_subj = '5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0'
+    poly_subj_1 = '5 * X^0 + 4 * X^1 = 4 * X^0'
     poly_subj_2 = '8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0'
-    poly_subj_3 = '5 + 4 * X + X^2= X^2'  # FAUX
+    poly_subj_3 = '5 + 4 * X + X^2 = X^2'
+
+    poly_slack = 'X^1 + X^5 = X^5'
+
+    poly_test = '5 * X^7 + 3 * X^1 + 3 * X^2 = 1 * X^0 + 0 * X^1'
 
     try:
-        print(pol := poly_subj_3)
+        print(pol := poly_test)
 
         poly = first_check(pol)
         left_side_expression, right_side_expression = poly.split('=')
@@ -29,7 +33,7 @@ if __name__ == "__main__":
         reduced_string = print_reduced_expression(non_null_dict)
         print(f"{reduced_string=}")
 
-        if not check_expression_solvability(non_null_dict):
+        if not check_expression_solvability(non_null_dict):  # TODO : Maybe put somewhere else so it can print the degree at the right time
             print("The polynomial degree is strictly greater than 2, I can't solve this equation")
 
         polynomial_solver = Polynomial(non_null_dict)
