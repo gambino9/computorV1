e89b94d4258dcaeeaa64dd8a02250e5bb49f8de1
Lamia <lboukrou@student.42.fr>
2022-08-05 12:49:09 +0200

    [REFACTOR]

--- polynomial.py
+++ polynomial.py
@@ -10,20 +10,11 @@ class Polynomial:
         self.reduced_expression = reduced_expression
 
         self.identify_a_b_c()
-        self.calculate_discriminant()
-        self.identify_equation_degree(self.reduced_expression)
-
-    def ft_power(self, x, y, z=None):
-        power = 1
-        # print(f'{x=}, {y=}')
-
-        for i in range(1, y + 1):
-            power = power * x
-        return power
+        self.discriminant = self.calculate_discriminant()
+        self.identify_equation_degree()
 
     def ft_square_root(self, x):
-        # sqrt = self.ft_power(x, 0.5)
-        sqrt = x ** 0.5  # TODO : May be forbidden in subject
+        sqrt = x ** 0.5
         return sqrt
 
     def identify_a_b_c(self) -> None:
@@ -32,41 +23,37 @@ class Polynomial:
         self.c = self.reduced_expression.get('0', 0)
 
     def calculate_discriminant(self):
-        discriminant = self.ft_power(self.b, 2) - (4 * self.a * self.c)
+        discriminant = self.b**2 - (4 * self.a * self.c)
         return discriminant
 
-    def identify_equation_degree(self, reduced_expression: dict) -> None:
-        print(f'Polynomial degree : {self.degree}')  # TODO : Find place where to print it properly
+    def identify_equation_degree(self) -> None:
+        print(f'Polynomial degree : {self.degree}')
         if self.degree == '0':
+            self.solution = 'No solution'
             print(f'This equation has no solution.')
         elif self.degree == '1':  # forme ax + b = 0
             self.solve_first_degree_equation()
         elif self.degree == '2':
-            discriminant = self.calculate_discriminant()
-            self.solve_second_degree_equation(discriminant)
+            self.solve_second_degree_equation()
         elif int(self.degree) > 2:
+            self.solution = 'unsolvable'
             print("The polynomial degree is strictly greater than 2, I can't solve it.")
 
-    def solve_second_degree_equation(self, discriminant):
-        if discriminant > 0:
-            self.solution = (-self.b + self.ft_square_root(discriminant)) / (2 * self.a)
-            self.solution_2 = (-self.b - self.ft_square_root(discriminant)) / (2 * self.a)
+    # aX + bX + c form
+    def solve_second_degree_equation(self):  # TODO : Use self.discriminant ?
+        if self.discriminant > 0:
+            self.solution = (-self.b + self.ft_square_root(self.discriminant)) / (2 * self.a)
+            self.solution_2 = (-self.b - self.ft_square_root(self.discriminant)) / (2 * self.a)
             print(f"The discriminant is strictly positive, the two solutions are :\n{self.solution}\n{self.solution_2}")
-        elif discriminant == 0:
+        elif self.discriminant == 0:
             self.solution = ((-self.b) / (2 * self.a))
-            print(f"The discriminant is equal to 0. The solution is : {self.solution}")
-        elif discriminant < 0:
-            self.solution = f"({-self.b} + i√{discriminant}) / {2 * self.a}"
-            self.solution_2 = f"({-self.b} - i√{discriminant}) / {2 * self.a}"
+            print(f"The discriminant is equal to 0. The solution is : \n{self.solution}")
+        elif self.discriminant < 0:
+            self.solution = f"({-self.b} + i√{self.discriminant}) / {2 * self.a}"
+            self.solution_2 = f"({-self.b} - i√{self.discriminant}) / {2 * self.a}"
             print(f"The discriminant is strictly negative, the two complex solutions are :\n{self.solution}\n{self.solution_2}")
 
-    # forme b * X + c
+    # bX + c form
     def solve_first_degree_equation(self):
-        if self.a == 0:  # P(x) = b
-            self.solution = (-self.c / self.b)
-            print(f"The solution is :\n{self.solution}")
-            # return f"The solution is :\n{self.solution}"
-        # Add here elif for special case when all reel number can be a solution (ax^0 = zx^0)
-        else:
-            self.solution = (-self.b) / self.a
-            print(f"The solution is :\n{self.solution}")  # TODO : check this maybe it's redundant or false
+        self.solution = (-self.c / self.b)
+        print(f"The solution is :\n{self.solution}")
